796481948f3049da501e6d22d1c73645cee7098c
Keith Burgie <krburgie@gmail.com>
2019-04-22 05:24:08 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,15 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('.tasty')
+  })
+}
 
 $(document).ready(function(){
 
